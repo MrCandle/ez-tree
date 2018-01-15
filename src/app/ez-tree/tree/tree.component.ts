ce5b8592becbedea5ea5ce193e2447c4a8cc3b4b
MrCandle <rodrigofvela@gmail.com>
2018-01-15 15:56:37 -0300

    Detect key down only on focus and blur

--- src/app/ez-tree/tree/tree.component.ts
+++ src/app/ez-tree/tree/tree.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, OnDestroy, SimpleChanges, Renderer2 } from '@angular/core';
+
+import { Node } from '../model/model';
 
 @Component({
   selector: 'app-tree',
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.css']
 })
-export class TreeComponent implements OnInit {
+export class TreeComponent implements OnInit, OnChanges, OnDestroy {
 
-  constructor() { }
+  hasFocus: boolean = false;
+  activeNode: number;
+  listenFunc: Function;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+
+  }
+
+  ngOnDestroy() {
+    this.listenFunc();
+  }
+
+  onTreeFocus() {
+    this.hasFocus = true;
+    this.listenFunc = this.renderer.listen('document', 'keydown', (evt) => {
+      if (evt.isTrusted) {
+        switch (evt.code) {
+          case 'ArrowRight': {
+            // expand node
+            break;
+          }
+          case 'ArrowLeft': {
+            // collapse node
+            break;
+          }
+          case 'ArrowUp': {
+            // select previous node
+            break;
+          }
+          case 'ArrowDown': {
+            // select next node
+            break;
+          }
+        }
+      }
+    })
+  }
+
+  onTreeBlur() {
+    this.hasFocus = false;
+    this.listenFunc();
+  }
 }
